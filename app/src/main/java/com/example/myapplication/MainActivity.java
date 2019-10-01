5ef0a818d4d97af494078c63500984427b2351ba
Frankenstein <Lucian@Frankensteins-MacBook-Pro.local>
2019-10-01 22:58:39 +1000

    csv export

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -15,6 +16,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -27,6 +29,8 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -57,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // label for position
         position = 0;
@@ -166,9 +169,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void addDevice(BluetoothDevice device, int new_rssi) {
-//        currentTime = Calendar.getInstance().getTime();
-//        System.out.println("currtime is: " + String.valueOf(currentTime));
-
         String address = device.getAddress();
 
         if (!mMap.containsKey(address)) {
@@ -190,5 +190,41 @@ public class MainActivity extends AppCompatActivity {
     //stop scanning
     public void stopScan(){
         mScanner.stop();
+    }
+
+    // https://developer.android.com/reference/android/support/v4/content/FileProvider
+    public void export(View view){
+
+
+        //generate data
+        StringBuilder data = new StringBuilder();
+        data.append("Time,Distance");
+        for(int i = 0; i<5; i++){
+            data.append("\n"+String.valueOf(i)+","+String.valueOf(i*i));
+        }
+
+        try{
+            //saving the file into device
+            FileOutputStream out = openFileOutput("data.csv", Context.MODE_PRIVATE);
+            out.write((data.toString()).getBytes());
+            System.out.println(data);
+            out.close();
+
+            //add exporting permission
+            Context context = getApplicationContext();
+            File filelocation = new File(getFilesDir(), "data.csv");
+            Uri path = FileProvider.getUriForFile(context, "com.example.myapplication.fileprovider", filelocation);
+            Intent fileIntent = new Intent(Intent.ACTION_SEND);
+            fileIntent.setType("text/csv");
+            fileIntent.putExtra(Intent.EXTRA_SUBJECT, "Data");
+            fileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            fileIntent.putExtra(Intent.EXTRA_STREAM, path);
+            startActivity(Intent.createChooser(fileIntent, "Send csv via email"));
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 }
